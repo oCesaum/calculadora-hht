334427dfa9de64b45e94cc21b189e5cd2f5ead52
César Augusto <97255173+oCesaum@users.noreply.github.com>
2023-07-08 22:15:19 -0300

    adding copy to clipboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,15 +18,23 @@ export default function Home() {
     setResult(0);
   }
 
+  function copyToClipboard() {
+      console.log('text', result)
+      var textField = document.createElement('textarea')
+      textField.innerText = JSON.stringify(Math.floor(result * 100) / 100)
+      document.body.appendChild(textField)
+      textField.select()
+      document.execCommand('copy')
+      textField.remove()
+  }
+
   return (
     <main className="bg-gradient-to-r from-gray-100 to-gray-300 w-screen min-h-screen grid place-content-center">
-      <div className="max-w-xs">
-        <h1 className="font-bold text-2xl uppercase text-center mb-10 text-slate-800">
-        Taxa de gravidade
-        </h1>
-      </div>
+      <h1 className="font-bold text-2xl uppercase text-center mb-10 text-slate-800">
+      Taxa de gravidade
+      </h1>
       <section className="flex flex-col justify-center items-center gap-5">
-        <div className="w-72 h-auto bg-white rounded-2xl shadow-xl border-4 border-gray-100">
+        <div className="w-72 h-auto bg-white rounded-2xl shadow-xl hover:shadow-xl transition-all border-4 border-gray-100">
           <div className="text-gray-700 font-semibold w-auto m-3 text-right space-y-2 py-2">
             Fórmua: <span className="text-sm">TG = T x 1.000.000 / H</span>
           </div>
@@ -36,7 +44,7 @@ export default function Home() {
               onChange={(e) => setTime(e.target.value)}
               type="number"
               name="time"
-              className="bg-gray-200 shadow-md rounded-2xl w-full h-12 text-black font-medium flex justify-center items-center outline-none text-center p-4"
+              className="bg-gray-200 shadow-md hover:shadow-lg transition-all rounded-2xl w-full h-12 text-black font-medium flex justify-center items-center outline-none text-center p-4"
               placeholder="Tempo em dias"
             ></input>
             <input
@@ -44,19 +52,19 @@ export default function Home() {
               onChange={(e) => setHour(e.target.value)}
               type="number"
               name="hours"
-              className="bg-gray-200 shadow-md rounded-2xl w-full h-12 text-black font-medium flex justify-center items-center outline-none text-center p-4"
+              className="bg-gray-200 shadow-md hover:shadow-lg rounded-2xl w-full h-12 text-black font-medium flex justify-center items-center outline-none text-center p-4"
               placeholder="Horas de exposição"
             ></input>
             <div className="flex gap-3">
               <button
                 onClick={() => clearCalculator()}
-                className="bg-red-500 shadow-md rounded-2xl w-full h-12 text-white font-medium text-xl flex justify-center items-center"
+                className="bg-red-500 shadow-md hover:shadow-lg rounded-2xl w-full h-12 text-white font-medium text-xl flex justify-center items-center"
               >
                 Limpar
               </button>
               <button
                 onClick={() => calculateTime()}
-                className="bg-green-500 shadow-md rounded-2xl w-full h-12 text-white font-medium text-xl flex justify-center items-center"
+                className="bg-green-500 shadow-md hover:shadow-lg rounded-2xl w-full h-12 text-white font-medium text-xl flex justify-center items-center"
               >
                 Calcular
               </button>
@@ -67,6 +75,12 @@ export default function Home() {
             <p className="text-gray-700 font-semibold w-auto text-center text-2xl uppercase">
               {result > 0 ? result : "aguardando"}
             </p>
+            <button
+                onClick={() => copyToClipboard()}
+                className="bg-blue-500 shadow-md hover:shadow-lg rounded-2xl w-full h-12 text-white font-medium text-xl flex justify-center items-center"
+              >
+                Copiar
+              </button>
             <div className="flex justify-center mt-5">
               <div className="w-20 h-1 bg-gray-100 rounded-l-xl rounded-r-xl"></div>
             </div>
